547e9a6334269956b56afd30d264ea5a9356285a
root <you@example.com>
2015-10-06 18:09:12 -0500

    pro

---
+++ FirstD/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'FirstD.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^logear/$', 'PeruCall.views.logear'),
+    url(r'^salir/$', 'PeruCall.views.salir'),
+    url(r'^menu/$', 'PeruCall.views.menu'),
+)
---
+++ PeruCall/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import *
+from django.template import RequestContext
+from django.contrib.auth import *
+from django.contrib.auth.models import Group, User
+from django.core import serializers
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core import serializers
+from django.core.urlresolvers import reverse
+from django.db.models import Max,Count
+from django.core.mail import send_mail
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+#from Perucall.models import *
+
+
+from django.db import transaction
+from django.contrib.auth.hashers import *
+from django.core.mail import send_mail
+from django.db import connection
+from django.utils.six.moves import range
+from django.http import StreamingHttpResponse
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import permission_required
+from django.views.decorators.csrf import csrf_exempt
+
+import xlrd
+import json 
+import csv
+import simplejson
+import xlwt
+import requests
+import os
+
+from datetime import datetime
+from django.contrib.auth import authenticate
+
+
+@login_required(login_url="/logear")
+
+def logear(request):
+
+	if request.user.is_authenticated():
+
+		return HttpResponseRedirect("/menu")
+
+	else:
+
+		if request.method == 'POST':
+
+			print request.POST
+
+			user = request.POST['username']
+			
+			psw = request.POST['contrasena']
+
+			user = authenticate(username=user, password=psw)
+
+		
+			if user is not None:
+
+				if user.is_active:
+
+					login(request, user)
+
+					return HttpResponseRedirect("/menu")
+
+			else:
+				return HttpResponseRedirect("/logear")
+		
+		else:
+
+			return render(request, 'logear.html',{})
+
+@login_required(login_url="/logear")
+
+def salir(request):
+
+	logout(request)
+	
+	return HttpResponseRedirect("/logear")
+
+
+def menu(request):
+
+		return render(request, 'menu.html',{})
