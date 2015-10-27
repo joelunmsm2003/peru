bf69269d1a601389eaf5a87aa2a18341978127a6
root <you@example.com>
2015-10-27 15:51:19 -0500

    last

--- FirstD/settings.py
+++ FirstD/settings.py
@@ -57,14 +57,22 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'PeruCall',
+    'rest_framework',
 )
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -101,9 +109,5 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
--- FirstD/urls.py
+++ FirstD/urls.py
@@ -1,15 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+
 
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'FirstD.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ingresar/$', 'PeruCall.views.ingresar'),
     url(r'^salir/$', 'PeruCall.views.salir'),
     url(r'^menu/$', 'PeruCall.views.menu'),    
 )
+
+
