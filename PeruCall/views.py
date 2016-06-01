b30e6e0c86f45e1513d945f70e96cd6da9d42952
Andy Jo <joelunmsm@gmail.com>
2016-06-01 13:01:57 -0500

    joel

--- PeruCall/views.py
+++ PeruCall/views.py
@@ -302,14 +302,19 @@ def teleoperador(request,id_agente):
 
 	id=request.user.id
 
+	base = Base.objects.filter(agente_id=id_agente,status=1).order_by('-id').values('id','telefono','orden','cliente','id_cliente','status_a','status_b','status_c','status_d','status_e','status_f','status_g','status_h','status','campania__nombre','resultado__name','resultado','campania__mxllamada','campania__mxllamada','campania__hombreobjetivo','campania__cartera__nombre')
+
+	dni = base[0]['id_cliente']
 
 	id_user = Agentes.objects.get(id=id_agente).user.id
 
 	url = AuthUser.objects.get(id=id).empresa.url
 
+
+
 	if id==id_user:
 
-		return render(request, 'screenagent.html',{'url':url})
+		return render(request, 'screenagent.html',{'url':url,'dni':dni})
 
 	else:
 
